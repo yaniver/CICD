98a198557415ddd9a31ac2c969d7b84777b501f5
yaniv eran <yaniver@gmail.com>
2019-12-01 15:32:31 +0200

    fix port number for docker run in launchJMeterDockerRun.sh

--- JMeter/launchJMeterDockerRun.sh
+++ JMeter/launchJMeterDockerRun.sh
@@ -1,7 +1,7 @@
 export timestamp=$(date +%Y%m%d_%H%M%S)
 export volume_path=~/CICD/JMeter
 export jmeter_path=/mnt/jmeter
-sudo docker run -p 80:80 \
+sudo docker run -p 8983:8983 \
 --volume "${volume_path}":${jmeter_path} \
 jmeter -n -Jcsv_staticParams=${jmeter_path}/static_parameters.txt \
 -Jmy_csv=${jmeter_path}/collectionsList.txt \
