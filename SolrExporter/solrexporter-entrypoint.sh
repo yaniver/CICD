0874c420e7fabf7709d9c02de1b7cf378423690e
yaniv eran <yaniver@gmail.com>
2019-12-01 18:20:34 +0200

    Update solrexporter shell

--- SolrExporter/solrexporter-entrypoint.sh
+++ SolrExporter/solrexporter-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/solr-exporter -p 8095 -z 10.16.250.247 -f ./conf/solr-exporter-config.xml
+./bin/solr-exporter -p 8095 -z 10.16.250.247:9983 -f ./conf/solr-exporter-config.xml -n 16
