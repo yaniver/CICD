595179469f25879e02f03de7b11fff03bd4dc592
Yaniv Eran 2 <yaniver@gmail.com>
2019-12-06 15:59:40 -0800

    fix

--- jenkinsPipelineShellScripts/solrExporterDeploy.sh
+++ jenkinsPipelineShellScripts/solrExporterDeploy.sh
@@ -4,7 +4,7 @@ JENKINS_HOME="/var/lib/jenkins"
 cd ${JENKINS_HOME}/CICD/SolrExporter
 sudo docker build -t solrexporter .
 solrExpoExist=$(sudo docker ps -f name=solrexporter | grep solrexporter)
-if [[ "$solrExpoExist" == "" ]]
+if [[ "$solrExpoExist" == "5" ]]
 then
 	sudo docker run --name solrexporter -d --restart always -p 8095:8095 solrexporter
 fi
