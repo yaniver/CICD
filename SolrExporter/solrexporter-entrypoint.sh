0bd2a3459be98d732a97e8a8f89b81c93c725fb3
yaniv eran <yaniver@gmail.com>
2019-12-08 17:01:46 +0200

    update solrexporter shell script with solr configuration instead of zk

--- SolrExporter/solrexporter-entrypoint.sh
+++ SolrExporter/solrexporter-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/solr-exporter -p 8095 -z 10.16.251.7:2181 -f ./conf/solr-exporter-config.xml -n 16
+./bin/solr-exporter -p 8095 -b http://10.16.251.22:8983/solr -f ./conf/solr-exporter-config.xml -n 16
