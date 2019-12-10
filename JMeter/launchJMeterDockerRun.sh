ef9ced015cbe8f89acc98e95f4d3271bbd556985
yaniv eran <yaniver@gmail.com>
2019-12-10 14:37:37 +0200

    set jmeter container with jmeter name

--- JMeter/launchJMeterDockerRun.sh
+++ JMeter/launchJMeterDockerRun.sh
@@ -4,7 +4,7 @@ JENKINS_HOME="/var/lib/jenkins"
 export timestamp=$(date +%Y%m%d_%H%M%S)
 export volume_path=${JENKINS_HOME}/CICD/JMeter
 export jmeter_path=/mnt/jmeter
-sudo docker run -p 8983:8983 \
+sudo docker run --name jmeter -p 8983:8983  \
 --volume "${volume_path}":${jmeter_path} \
 jmeter -n -Jcsv_staticParams=${jmeter_path}/static_parameters.txt \
 -Jmy_csv=${jmeter_path}/collectionsList.txt \
