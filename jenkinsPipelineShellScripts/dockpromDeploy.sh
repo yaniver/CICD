318113ae3f26bdd6e34421a16b1c8b6fede68eb2
yaniv eran <yaniver@gmail.com>
2019-12-10 14:29:42 +0200

    add SMTP env config into dockprom.zip

--- jenkinsPipelineShellScripts/dockpromDeploy.sh
+++ jenkinsPipelineShellScripts/dockpromDeploy.sh
@@ -2,7 +2,10 @@
 
 JENKINS_HOME="/var/lib/jenkins"
 cd ${JENKINS_HOME}/CICD
+#online - internet connection
 sudo git clone https://github.com/stefanprodan/dockprom
+#offline
+sudo unzip -o dockprom.zip -d dockprom
 sudo cp ${JENKINS_HOME}/CICD/dockerpromModification/GrafanaDashboardExtra/*.json ${JENKINS_HOME}/CICD/dockprom/grafana/provisioning/dashboards
 sudo cp ${JENKINS_HOME}/CICD/dockerpromModification/Prometheus/*.yml ${JENKINS_HOME}/CICD/dockprom/prometheus
 cd dockprom
