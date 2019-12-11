1f75893660e0448d5a708170da0692496c149748
root <yaniver@gmail.com>
2019-12-11 17:03:11 +0200

    fix grafanaalert script

--- jenkinsPipelineShellScripts/grafanaAlert.sh
+++ jenkinsPipelineShellScripts/grafanaAlert.sh
@@ -3,9 +3,10 @@
 alertExist="$(curl  -H "Authorization: Basic YWRtaW46YWRtaW4=" http://localhost:3000/api/alerts | grep -Eoi "name\":\"Container CPU Usage alert\",\"state\":\"Alerting\"")"
 if [[ "$alertExist" == "" ]]
 then
+	exit 1
+else
         printf "\n\n"
 	echo "No critical Alert exist for stopping JMeter load scenario"
         printf "\n"
-else
-	exit 1
+	exit 0
 fi
