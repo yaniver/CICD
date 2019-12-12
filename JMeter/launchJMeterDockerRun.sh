63a93dec00f696de54f7c3554e14d48c2338aff8
root <yaniver@gmail.com>
2019-12-12 13:57:34 +0200

    Set 5 threads for JMeter

--- JMeter/launchJMeterDockerRun.sh
+++ JMeter/launchJMeterDockerRun.sh
@@ -8,6 +8,6 @@ sudo docker run --name jmeter -p 8983:8983  \
 --volume "${volume_path}":${jmeter_path} \
 jmeter -n -Jcsv_staticParams=${jmeter_path}/static_parameters.txt \
 -Jmy_csv=${jmeter_path}/collectionsList.txt \
--JThreads=1 \
+-JThreads=5 \
 -t ${jmeter_path}/SolrJ.jmx \
 -l ${jmeter_path}/tmp/result_${timestamp}.jtl -j ${jmeter_path}/tmp/jmeter_${timestamp}.log
