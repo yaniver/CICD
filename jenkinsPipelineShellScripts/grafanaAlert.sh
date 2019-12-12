278996da6a3719f950fedb1055fd3bae567b98af
root <yaniver@gmail.com>
2019-12-12 10:35:27 +0200

    fix grafanaAlert script logic

--- jenkinsPipelineShellScripts/grafanaAlert.sh
+++ jenkinsPipelineShellScripts/grafanaAlert.sh
@@ -2,18 +2,18 @@
 
 alertExist="$(curl  -H "Authorization: Basic YWRtaW46YWRtaW4=" http://localhost:3000/api/alerts | grep -Eoi "name\":\"Container CPU Usage alert\",\"state\":\"Alerting\"")"
 
-for i in {1..99999999}
-	do
+condition="true"
+while [[ "$condition" == "true" ]]
+do
 	if [[ "$alertExist" == "" ]]
 	then
 		printf "\n\n"
 		echo "No critical Alert exist for stopping JMeter load scenario"
 		printf "\n"
-		exit 0
 	else
 		echo "Critical Alert exist, fail stage"
+		condition="false"
 		exit 1
-		break
 	fi
 	sleep 1m
 done
